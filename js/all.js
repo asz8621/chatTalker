3268c243f23a57a97d175b94ea2a3ec10d21838c
Nielsen <asz718460@gmail.com>
2022-05-29 19:42:43 +0800

    [modify] index & 新增 goTop 按鈕

--- js/all.js
+++ js/all.js
@@ -1,24 +1,51 @@
-// slick
-$('.slick-evaluation').slick({
-  dotsClass: 'slick-dots d-flex justify-content-center align-items-center flex-row pos-s',
-  dots: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 1,
-      }
+$(document).ready(function() {
+  // navbar
+  if($(window).scrollTop() > 50) {
+    $('.navbar').addClass('scroll')
+    $('.goTop').fadeIn(250)
+  } else {
+    $('.navbar').removeClass('scroll')
+		$('.goTop').fadeOut(250)
+  }
+  $(window).scroll(function() {
+    if($(window).scrollTop() > 50) {
+      $('.navbar').addClass('scroll')
+      $('.goTop').fadeIn()
+    } else {
+      $('.navbar').removeClass('scroll')
+		  $('.goTop').fadeOut()
     }
-  ]
-});
+  })
+
+  // goTop
+  $('.goTop').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000)
+  })
+
+  // slick
+  $('.slick-evaluation').slick({
+    dotsClass: 'slick-dots d-flex justify-content-center align-items-center flex-row pos-s',
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+})
